e6ea076b994d961624971adf78e390a6b6080ba8
luxgood <r.omitsuki@gmail.com>
2019-02-08 00:28:42 +0100

    spring boot initializr

---
+++ src/main/java/com/luxgood/tauproject/TauprojectApplication.java
@@ -0,0 +1,14 @@
+package com.luxgood.tauproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TauprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TauprojectApplication.class, args);
+	}
+
+}
+
